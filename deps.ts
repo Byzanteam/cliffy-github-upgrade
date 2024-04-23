03dfad5ce6c196081c0ea1976c300f2b072ac226
Dane Wang <dwalters.maruko@gmail.com>
2024-04-23 13:00:00 +0800

    chore: add dependency

---
+++ deps.ts
@@ -0,0 +1,13 @@
+export {
+  Command,
+  ValidationError,
+} from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+export { Table } from "https://deno.land/x/cliffy@v1.0.0-rc.3/table/mod.ts";
+export {
+  bold,
+  brightBlue,
+  cyan,
+  green,
+  red,
+  yellow,
+} from "https://deno.land/std@0.221.0/fmt/colors.ts";
